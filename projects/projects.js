0b52577ae54cca7ef604540bf2204f3b95318ed6
David S Oh <D.SeungbynOh@gmail.com>
2025-05-01 23:57:37 -0700

    yer

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadProjects() {
   const projects = await fetchJSON('../lib/projects.json');
@@ -15,3 +16,5 @@ async function loadProjects() {
 }
 
 loadProjects();
+
+
