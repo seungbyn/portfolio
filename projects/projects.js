ca964a85bc1c044b5f953c15d65e18c9ad4f4250
David S Oh <D.SeungbynOh@gmail.com>
2025-05-02 00:25:56 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -17,11 +17,9 @@ async function loadProjects() {
 
 loadProjects();
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
     startAngle: 0,
     endAngle: 2 * Math.PI,
-});
+  });
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-
 
