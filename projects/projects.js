670788942f6e5f6a17f0915f9f56a259783929fa
David S Oh <D.SeungbynOh@gmail.com>
2025-05-02 01:13:17 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,11 +1,10 @@
 import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-import { fetchJSON, renderProjects } from '../global.js';
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadProjects() {
   const projects = await fetchJSON('../lib/projects.json');
+  console.log('Loaded projects:', projects);
   const projectsContainer = document.querySelector('.projects');
   const searchInput = document.querySelector('.searchBar');
 
