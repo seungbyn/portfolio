9ca2e9ceba4981fbb1ebe0ee22bc0fa68f93c0bc
David S Oh <D.SeungbynOh@gmail.com>
2025-05-10 22:35:13 -0700

    a

--- meta/main.js
+++ meta/main.js
@@ -9,7 +9,6 @@ async function loadData() {
         date: new Date(row.date + 'T00:00' + row.timezone),
         datetime: new Date(row.datetime),
     }));
-  
     return data;
 }
 
@@ -19,7 +18,7 @@ function processCommits(data) {
         .map(([commit, lines]) => {
             let first = lines[0];
             let { author, date, time, timezone, datetime } = first;
-  
+
             let ret = {
                 id: commit,
                 url: 'https://github.com/vis-society/lab-7/commit/' + commit,
@@ -31,14 +30,14 @@ function processCommits(data) {
                 hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
                 totalLines: lines.length,
             };
-  
+
             Object.defineProperty(ret, 'lines', {
                 value: lines,
                 enumerable: false,
                 writable: true,
                 configurable: true,
             });
-  
+
             return ret;
         });
 }
@@ -50,8 +49,6 @@ function renderTooltipContent(commit) {
     const author = document.getElementById('commit-author');
     const lines = document.getElementById('commit-lines');
 
-    if (!commit) return;
-
     link.href = commit.url;
     link.textContent = commit.id;
     date.textContent = commit.datetime?.toLocaleDateString('en', { dateStyle: 'full' });
@@ -62,22 +59,24 @@ function renderTooltipContent(commit) {
 
 function updateTooltipVisibility(isVisible) {
     const tooltip = document.getElementById('commit-tooltip');
-    tooltip.hidden = !isVisible;
+    tooltip.style.opacity = isVisible ? 1 : 0;
 }
 
 function updateTooltipPosition(event) {
     const tooltip = document.getElementById('commit-tooltip');
+    const padding = 10;
     const tooltipWidth = tooltip.offsetWidth;
     const tooltipHeight = tooltip.offsetHeight;
 
-    let left = event.clientX + 15;
-    let top = event.clientY + 15;
+    let left = event.clientX + padding;
+    let top = event.clientY + padding;
 
+    // Prevent overflow
     if (left + tooltipWidth > window.innerWidth) {
-        left = event.clientX - tooltipWidth - 15;
+        left = event.clientX - tooltipWidth - padding;
     }
     if (top + tooltipHeight > window.innerHeight) {
-        top = event.clientY - tooltipHeight - 15;
+        top = event.clientY - tooltipHeight - padding;
     }
 
     tooltip.style.left = `${left}px`;
@@ -116,33 +115,6 @@ function renderScatterPlot(data, commits) {
         .range([usableArea.bottom, usableArea.top])
         .nice();
 
-    // Gridlines
-    const gridlines = svg
-        .append('g')
-        .attr('class', 'gridlines')
-        .attr('transform', `translate(${usableArea.left}, 0)`);
-
-    gridlines.call(
-        d3.axisLeft(yScale).tickSize(-usableArea.width).tickFormat('').ticks(24)
-    );
-
-    // Axes
-    const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat('%b %d'));
-    const yAxis = d3.axisLeft(yScale).tickFormat((d) => `${String(d % 24).padStart(2, '0')}:00`);
-
-    svg
-        .append('g')
-        .attr('transform', `translate(0, ${usableArea.bottom})`)
-        .attr('class', 'x-axis')
-        .call(xAxis);
-
-    svg
-        .append('g')
-        .attr('transform', `translate(${usableArea.left}, 0)`)
-        .attr('class', 'y-axis')
-        .call(yAxis);
-
-    // Scatterplot Dots
     const dots = svg.append('g').attr('class', 'dots');
 
     dots
@@ -154,20 +126,23 @@ function renderScatterPlot(data, commits) {
         .attr('r', 5)
         .attr('fill', (d) => (d.hourFrac >= 6 && d.hourFrac <= 18 ? '#ffa726' : '#42a5f5'))
         .attr('opacity', 0.8)
-        .on('mouseenter', (event, commit) => {
+        .on('mouseenter', function (event, commit) {
+            d3.select(this).attr('r', 7).attr('fill', '#ff7043');
             renderTooltipContent(commit);
             updateTooltipVisibility(true);
             updateTooltipPosition(event);
         })
-        .on('mousemove', updateTooltipPosition)
-        .on('mouseleave', () => {
+        .on('mousemove', function (event) {
+            updateTooltipPosition(event);
+        })
+        .on('mouseleave', function () {
+            d3.select(this).attr('r', 5).attr('fill', (d) => (d.hourFrac >= 6 && d.hourFrac <= 18 ? '#ffa726' : '#42a5f5'));
             updateTooltipVisibility(false);
         });
 }
 
 
-let data = await loadData();
-let commits = processCommits(data);
+const data = await loadData();
+const commits = processCommits(data);
 renderScatterPlot(data, commits);
-
 
