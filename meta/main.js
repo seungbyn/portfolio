35a5c1ba795cc4e038577c53b268928cca65e7c8
David S Oh <D.SeungbynOh@gmail.com>
2025-05-10 21:51:30 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -15,22 +15,34 @@ async function loadData() {
 
 function processCommits(data) {
     return d3
-      .groups(data, (d) => d.commit)
-      .map(([commit, lines]) => {
-        // Each 'lines' array contains all lines modified in this commit
-        // All lines in a commit have the same author, date, etc.
-        // So we can get this information from the first line
-        let first = lines[0];
+        .groups(data, (d) => d.commit)
+        .map(([commit, lines]) => {
+            let first = lines[0];
+            let { author, date, time, timezone, datetime } = first;
   
-        // What information should we return about this commit?
-        return {
-          id: commit,
-          // ... what else?
-        };
-      });
-  }
+            let ret = {
+                id: commit,
+                url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+                author,
+                date,
+                time,
+                timezone,
+                datetime,
+                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+                totalLines: lines.length,
+            };
+  
+            Object.defineProperty(ret, 'lines', {
+                value: lines,
+                enumerable: false, // Do not include in console output
+                writable: true,
+                configurable: true,
+            });
+  
+            return ret;
+        });
+}
   
 let data = await loadData();
 let commits = processCommits(data);
-    
-console.log(commits)
+console.log(commits);
